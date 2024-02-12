30667fbe00ed132424ff2746714b95fc61eb2739
Jakub <banaszek.jakub1@gmail.com>
2024-02-12 20:43:21 +0100

    add new features

--- password_validator/gui.py
+++ password_validator/gui.py
@@ -1,14 +1,25 @@
 from tkinter import *
-from hashpassword import Password
+from hashpassword import Login
 
 
 root = Tk()
 root.geometry('300x300')
 
+# login
+text = Label(root, text='Entry nick')
+text.pack()
+login_entry = Entry()
+login_entry.pack()
+button_login = Button(text="validate", command=lambda: Login.validate_nick(login_entry.get()))
+button_login.pack()
+
+# password
+text_password = Label(root, text='Entry password')
+text_password.pack()
 password_entry = Entry(root)
 password_entry.pack()
-button = Button(text="validate", command=lambda: Password.validate(password_entry.get()))
-button.pack()
+button_password = Button(text="validate", command=lambda: Login.validate_password(password_entry.get()))
+button_password.pack()
 
 
 root.mainloop()
--- password_validator/hashpassword.py
+++ password_validator/hashpassword.py
@@ -1,24 +1,23 @@
 import bcrypt
+from database import Database
 
-class Password():
+class Login():
+    def __init__(self, password, login):
+        self.password = password
+        self.login = login
     @staticmethod
-    def validate(password):
+    def validate_password(self, hash_value):
         hash_value = b'$2b$12$uZv8YagUvbYkNlj.RWquDOeprxnBGFkxKydlL7.jw3xrF5p89IBKC'
-        password = bytes(password, encoding='utf-8')
-        if bcrypt.checkpw(password, hash_value):
+        password = bytes(self.password, encoding='utf-8')
+        if bcrypt.checkpw(self.password, hash_value):
             print('Login successful')
+
         else:
             print('Invalid password')
 
-
-"""password = b"thisismypassword"
-hashed = bcrypt.hashpw(password, bcrypt.gensalt())
-print(hashed)
-
-entered_password = input('Enter password to login')
-entered_password = bytes(entered_password, encoding = 'utf-8')
-
-if bcrypt.checkpw(entered_password, hashed):
-    print('Login successful')
-else:
-    print('Invalid password')"""
+    @staticmethod
+    def validate_login(self):
+        if bcrypt.checkpw(self.login, self.):
+            print('Login successful')
+        else:
+            print('Invalid password')
